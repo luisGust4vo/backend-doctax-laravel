9d317e0a56d05afd6cb08f870adf901b9cfe6448
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-02-17 00:17:08 -0300

    Test:Criacao de test unitario pras funcoes

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,15 +3,24 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    public function registrar_usuario(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_usuario_pode_se_cadastrar()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'Werton VG',
+            'email' => 'test@example.com',
+            'password' => '123123',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@example.com'
+        ]);
+
+        $response->assertStatus(201);
     }
 }
