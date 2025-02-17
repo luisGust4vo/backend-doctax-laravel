b820d5df8903eb75f21e0f1c414ba903fa0b107d
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-02-16 22:24:37 -0300

    Feat:Encapsulamento de autenticador para as rotas com permissao apos estar logado

--- routes/user.php
+++ routes/user.php
@@ -1,17 +1,19 @@
 <?php
+
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
 
-Route::prefix('user')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('{id}', [UserController::class, 'show']);
-    Route::put('{id}', [UserController::class, 'update']);
-    Route::delete('{id}', [UserController::class, 'destroy']);
-    Route::any('{any}', function () {
-        return response()->json([
-            'error' => 'Rota não encontrada dentro do prefixo /user'
-        ], Response::HTTP_NOT_FOUND);
-    })->where('any', '.*');
-
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('user')->group(function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::get('{id}', [UserController::class, 'show']);
+        Route::put('{id}', [UserController::class, 'update']);
+        Route::delete('{id}', [UserController::class, 'destroy']);
+        Route::any('{any}', function () {
+            return response()->json([
+                'error' => 'Rota não encontrada dentro do prefixo /user'
+            ], Response::HTTP_NOT_FOUND);
+        })->where('any', '.*');
+    });
 });
