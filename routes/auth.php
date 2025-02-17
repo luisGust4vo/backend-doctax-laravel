926b8fb3a75cf831f31aff88c782c0a37e8a8026
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-02-16 22:07:43 -0300

    Feat:Criacao de api de login para os demais partes

--- routes/auth.php
+++ routes/auth.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 // Route::middleware('auth:api')->group(function () {
 //     Route::post('logout', [AuthController::class, 'logout']);
